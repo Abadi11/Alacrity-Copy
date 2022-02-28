bdcc7df95f0c9d43f88d80f8c5e5ff7af0c896a4
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-02-28 10:31:59 +0000

    backend setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
+const express = require("express"); 
+const cors = require("cors");
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, ()=>{
+ console.log(`Alacrity server is now gravitating on Port: ${port}`);
+})
