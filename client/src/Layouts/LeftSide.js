120cdfe46295f5b3b79e95263059864eea3a24fa
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-03-16 19:17:05 +0000

    update land page for left side

--- client/src/Layouts/LeftSide.js
+++ client/src/Layouts/LeftSide.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import Logo from '../img/new_log_svg.svg'
-import AnalyticsLogo from '../img/pie-chart.png'
+import React from 'react';
+import Logo from '../img/new_log_svg.svg';
+import AnalyticsLogo from "../img/pie-chart-24.png";
+import SettingsLogo from '../img/settings-24.png';
 
 function LeftSide() {
   return (
     <div className="left-side-container">
       <p>leftSide</p>
       <div className="img-logo-container">
-        <img src={Logo} alt="Alacrity-Logo"/>
+        <img src={Logo} alt="Alacrity-Logo" />
       </div>
-      <div className='analytics-container'>
-        <img src={AnalyticsLogo} alt="Analytics-Logo" className='topic-logo'/>
+      <div className="bar-container">
+        <img src={AnalyticsLogo} alt="Analytics-Logo" className="topic-logo" />
         <h3>Analytics</h3>
+      </div>
+      <div className="bar-container">
+        <img src={SettingsLogo} alt="Settings-Logo" className="topic-logo" />
+        <h3>Settings</h3>
       </div>
     </div>
   );
