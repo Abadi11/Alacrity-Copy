b39caacfcad8c9c8915591f0aace9bd58e219c82
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-04-12 17:12:06 +0100

    nav links in the middle css

--- client/src/Layouts/MiddleSide.js
+++ client/src/Layouts/MiddleSide.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import data from '../data/data.json'
 
 function MiddleSide() {
   return (
     <div className="middle-side-container">
-      <h3>Welcome Back name!</h3>
+      <div className="middle-side-right">
+        <h3>Welcome Back name!</h3>
+        <h3>
+          <strong>You've got {data.length} Tasks</strong>
+        </h3>
+        <div className="nav_container">
+          <ul className="nav_links">
+            <li>
+              <a href="#"> Today </a>
+            </li>
+            <li>
+              <a href="#"> Tomorrow </a>
+              <li>
+                <a href="#"> Later </a>
+              </li>
+              <li>
+                <a href="#"> Archived </a>
+              </li>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
